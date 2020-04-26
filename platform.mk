d5ead4621fabeb690d2ce0adf30e36926eb4f55d
Marijn Suijten <marijns95@gmail.com>
2020-04-26 14:47:17 +0200

    audio_policy: Move common "primary" configuration to separate file
    This will reduce duplication when the new audio HAL and A2DP offloading
    come online, that both require duplication of the toplevel audio policy
    file extended with the appropriate routes.
    
    Signed-off-by: Marijn Suijten <marijns95@gmail.com>

--- platform.mk
+++ platform.mk
@@ -82,7 +82,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
     $(SONY_ROOT)/vendor/etc/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(SONY_ROOT)/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+    $(SONY_ROOT)/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    $(SONY_ROOT)/vendor/etc/common_primary_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/common_primary_audio_policy_configuration.xml
 
 # Media
 PRODUCT_COPY_FILES += \
