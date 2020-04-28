db8f1fc9c1b864958bb0d09ad66eeeb856dbc5dc
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-04-28 18:33:51 +0200

    platform: Add permissions for NFC eSE and UICC
    This platform features an embeddedSecureElement enabled NFC
    chip.

--- platform.mk
+++ platform.mk
@@ -78,6 +78,11 @@ PRODUCT_COPY_FILES += \
      frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
      frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# NFC eSE Permissions
+PRODUCT_COPY_FILES += \
+     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_OUT_COPY_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt \
