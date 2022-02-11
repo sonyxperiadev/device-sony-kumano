86298840002e2c773c13a52f8cdaccaebbe39577
Pavel Dubrova <pashadubrova@gmail.com>
2022-02-11 11:28:42 +0200

    PlatformConfig: Remove msm_drm.blhack_dsi_display0 cmdline
    The msm_drm.blhack_dsi_display0 cmdline hack should only be used if the
    bootloader does not set the value for the msm_drm.dsi_display0 cmdline
    to be the display panel node name or sets it incorrectly (sets its label
    name instead of the actual node name for example). In our case, the Kumano
    platform bootloader sets the correct value of the msm_drm.dsi_display0
    cmdline.
    
    By having this useless hack we run the risk of having a broken display
    panel due to its misconfiguration, hence removing this cmdline property
    to avoid this behavior.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -29,7 +29,6 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=1d84000.ufshc
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
-BOARD_KERNEL_CMDLINE += msm_drm.blhack_dsi_display0=dsi_panel_somc_kumano_cmd:config0
 
 # Serial console
 #BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0xa90000
